cf71295508ff4efefa85ada9a962fc0fb32e6f8f
chenwei <chenwei@chenweideMacBook-Pro.local>
2018-06-03 23:24:41 +0800

    详情页mac

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -28,6 +28,12 @@
                 <h1 class="title">商品信息</h1>
                 <p class="text">{{food.info}}</p>
             </div>
+            <split></split>
+            <div class="rating">
+                <h1 class="title">商品评价</h1>
+                <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                      :ratings="food.ratings"></ratingselect>
+            </div>
         </div>
     </div>
 </template>
@@ -37,6 +43,8 @@ import BScroll from 'better-scroll';
 import Vue from 'vue';
 import cartcontrol from 'components/cartcontrol/cartcontrol';
 import split from 'components/split/split';
+import ratingselect from 'components/ratingselect/ratingselect'; 
+const ALL = 2;
 
 export default {
     props: {
@@ -46,12 +54,21 @@ export default {
     },
     data() {
         return {
-            showFlag: false
+            showFlag: false,
+            selectType: ALL,
+            onlyContent: true,
+            desc: {
+                all: '全部',
+                positive: '推荐',
+                negative: '吐槽'
+            }
         }
     },
     methods: {
         show() {
            this.showFlag = true;
+           this.selectType = ALL;
+           this.onlyContent = true;
            this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$els.food, {
@@ -75,6 +92,7 @@ export default {
     },
     components: {
         cartcontrol,
+        ratingselect,
         split
     }
 }
@@ -178,7 +196,14 @@ export default {
             line-height: 24px
             padding: 0 8px
             font-size: 12px
-            color: rgb(77, 85, 93)        
+            color: rgb(77, 85, 93)
+        .rating
+            padding-top: 18px
+            .title
+                line-height: 14px
+                margin-left: 18px
+                font-size: 14px
+                color: rgb(7, 17, 27)        
 </style>
 
 
